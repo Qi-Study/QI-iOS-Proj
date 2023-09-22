bd5b83138fef8871bf7d18951bd8b42668008f69
박준현 <me@4rne5.dev>
2023-09-22 12:06:33 +0900

    Initial Commit

---
+++ QI-iOS-Proj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QI-iOS-Proj
+//
+//  Created by 4rNe5 on 2023/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QI-iOS-Proj/QI_iOS_ProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  QI_iOS_ProjApp.swift
+//  QI-iOS-Proj
+//
+//  Created by 4rNe5 on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct QI_iOS_ProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
